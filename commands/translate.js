c11a8e237107c3ab11603c7c7412d55ee80cbcd5
Filip Glura <lockontargt@gmail.com>
2021-10-19 22:45:39 +0200

    feat: cleaned unused packages and added detection of the selected button

--- commands/translate.js
+++ commands/translate.js
@@ -1,20 +1,21 @@
 const { latin, galactic } = require("./../utils/alphabets.js")
-const { MessageActionRow, MessageButton } = require("discord.js")
+const { MessageActionRow, MessageButton, ButtonInteraction } = require("discord.js")
 const { SlashCommandBuilder } = require("@discordjs/builders")
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("translate")
-        .setDescription("translate"),
-
+        .setDescription("translate to galactic and vice versa")
+        .addStringOption(option => option.setName('text').setDescription('Text to translate')),
+    
     async execute(client, interaction) {
         let translatedText = [];
+        const text = interaction.options.getString('text');
         const translateLetter = (letter, method) => (
             method === "togalactic" 
             ? galactic[latin.indexOf(letter)] 
             : latin[galactic.indexOf(letter)]
         )
-
         const row = new MessageActionRow().addComponents(
             new MessageButton()
                 .setCustomId("to-galactic")
@@ -27,28 +28,30 @@ module.exports = {
                 .setLabel("To latin")
                 .setStyle("PRIMARY")
         )
-
         interaction.reply({
             content: " in which side?",
             components: [row],
             ephemeral: true,
         })
 
-
-        // if (args[1] === "togalactic") {
-        //     for(let i=0; i<=furtherArgs.length-1; i++) {
-        //         furtherArgs[i].search(/[a-zA-Z]/g) === 0
-        //         ? translatedText.push(translateLetter(furtherArgs[i], "togalactic"))
-        //         : translatedText.push(furtherArgs[i])
-        //     }
-        // } else {
-        //     for(let i=0; i<=furtherArgs.length-1; i++) {
-        //         furtherArgs[i].search(/[a-zA-Z,!?]/g) === 0
-        //         ? translatedText.push(translateLetter(furtherArgs[i], "toalphabet"))
-        //         : translatedText.push(" ") 
-        //     }
-        // }
-        // message.reply(translatedText.join(""))
-
+        const collector = interaction.channel.createMessageComponentCollector({ componentType: 'BUTTON' });
+        collector.on('collect', async button => {
+            text.split("")
+            if(button.customId === "to-galactic") {
+                for(let i=0; i<=text.length-1; i++) {
+                    text[i].search(/[a-zA-Z]/g) === 0
+                    ? translatedText.push(translateLetter(text[i], "togalactic"))
+                    : translatedText.push(text[i])
+                }
+                interaction.followUp({ content: translatedText.join("") })
+            } else {
+                for(let i=0; i<=text.length-1; i++) {
+                    text[i].search(/[a-zA-Z]/g) === 0
+                    ? translatedText.push(translateLetter(text[i], "toalphabet"))
+                    : translatedText.push(" ") 
+                }
+                interaction.followUp({ content: translatedText.join("") })
+            }
+        })
     }
 }
