7d62013bf192bfc68b9d4ee4e97fab5319278907
Filip Glura <lockontargt@gmail.com>
2021-10-23 11:24:12 +0200

    feat: conversion of a help command into a slash command

--- commands/help.js
+++ commands/help.js
@@ -1,20 +1,25 @@
 const Discord = require('discord.js');
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
-    name : `help`,
-    description : `All available commands`,
-
-    execute(message, args, client) {
+    data: new SlashCommandBuilder()
+        .setName("help")
+        .setDescription("All available commands"),
+    
+    async execute(client, interaction) {
         const info = new Discord.MessageEmbed()
-            .setColor(`#ffffff`)
-            .setAuthor(`TFTF BOT`, `https://i.imgur.com/J4O9NsF.jpg`, `https://github.com/TARGEToff/tftf-bot`)
-            .setThumbnail('https://i.imgur.com/J4O9NsF.jpg')
-            .setTitle(`All commands`)
+            .setColor("#ffffff")
+            .setAuthor("TFTF BOT", "https://i.imgur.com/J4O9NsF.jpg", "https://github.com/TARGEToff/tftf-bot")
+            .setThumbnail("https://i.imgur.com/J4O9NsF.jpg")
+            .setTitle("All commands")
             .addFields(
-                { name: `!info`, value: `Bot information`},
-                { name: `!mute [user] [time]`, value: `Mute a user`},
-                { name: `!unmute [user]`, value: `Unmute a user`},
-                { name: `!play [title]`, value: `Play a song`})
-        message.channel.send(info);
+                { name: "!info", value: "Bot information"},
+                { name: "!mute [user] [time]", value: "Mute a user"},
+                { name: "!unmute [user]", value: "Unmute a user"},
+                { name: "!translate [text]", value: "translate to galactic and vice versa"})
+        interaction.reply({ 
+                embeds: [info], 
+                ephemeral: true 
+        })
     }
 };
