07b5fe6b2a959762d87e31a29f20f0560340afa1
Filip Glura <lockontargt@gmail.com>
2021-10-29 09:19:50 +0200

    feat: added postgresql database and prisma

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
 const { Client } = require(`discord.js`);
+const { PrismaClient } = require('@prisma/client');
 const commandHandler = require(`./command_handler.js`);
 const chalk = require("chalk");
 require("dotenv").config();
 
+const prisma = new PrismaClient()
 const client = new Client({
     intents: ["GUILDS", "GUILD_MESSAGES"],
     allowedMentions: { parse: ["users", "roles"], repliedUser: true },
@@ -14,9 +16,30 @@ client.once("ready", () => {
     console.log(chalk.green(`${client.user.tag} working!`));
 
     console.log("I belong to:");
-    client.guilds.cache.forEach((guild) => {
+    client.guilds.cache.forEach(async(guild) => {
         console.log(`${chalk.blue(guild.name)} | ${chalk.green(guild.id)}`);
+        // await prisma.servers.create({
+        //     data: {
+        //         id: Number(guild.id),
+        //         name: guild.name,  
+        //     },
+        // })
     });
 });
+
+const main = async () => {
+    const allServers = await prisma.servers.findMany({
+        select: { name: true }
+    })
+    console.log(allServers)
+}
+
+main()
+  .catch((e) => {
+    throw e
+})
+  .finally(async () => {
+    await prisma.$disconnect()
+})
 
 client.login(process.env.D_TOKEN);
